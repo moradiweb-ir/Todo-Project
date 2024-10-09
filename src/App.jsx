8b5e83daec9eb416c5829103ff03c87c86c544b6
moradiweb <moradiweb.ir@gmail.com>
2024-10-08 22:17:58 -0700

    start project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react'
+import './App.css'
+import Todo from './comp/Todo Box/Todo'
+
+function App() {
+
+
+  return (
+    // <StrictMode>
+ <Todo/>
+    // </StrictMode>
+
+   
+  )
+}
+
+export default App
+ 
